859bdf0512ae551535737874f0e66efabc0d7493
RangerMauve <RangerMauve@hotmail.com>
2014-10-08 19:42:01 -0400

    Set CORS header

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ var fs = require('fs');
 
 app.listen(process.env.PORT || 8080);
 
+io.set("origin", "*");
+
 io.on('connection', function(socket) {
 	socket.on('speak', function(data) {
 		var voice = data.voice

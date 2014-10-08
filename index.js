841f0c3b5aaa56a88421dd5970a32e984548c849
RangerMauve <RangerMauve@hotmail.com>
2014-10-08 19:49:13 -0400

    Added express

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
-var app = require('http').createServer(function() {});
-var io = require('socket.io')(app);
+var express = require("express");
+var app = express();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
 var fs = require('fs');
 
 app.listen(process.env.PORT || 8080);
 
-io.set("origin", "*");
+app.disable('x-powered-by');
+
+app.use(function(req, res, next) {
+	res.setHeader('Access-Control-Allow-Origin', "http://" + req.headers.host + ':8000');
+
+	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+	res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+	next();
+});
 
 io.on('connection', function(socket) {
 	socket.on('speak', function(data) {

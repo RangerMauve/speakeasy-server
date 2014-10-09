3f1ac92b3bdec5e417610b3ad0e6dde833c6f500
RangerMauve <RangerMauve@hotmail.com>
2014-10-08 21:02:14 -0400

    Set up shoe

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 var express = require("express");
 var app = express();
 var server = require('http').Server(app);
+var shoe = require("shoe");
+var events = new(require("events").EventEmitter);
+var par = require("par");
+
+var nodes = [];
 
 app.disable('x-powered-by');
 
@@ -12,5 +17,31 @@ app.use(function(req, res, next) {
 	next();
 });
 
-console.log("Listening")
+var sock = shoe(function(stream) {
+	var node = dnode({
+		say: say_all
+	});
+	add_user(node);
+	stream.on("end", par(remove_user, node));
+	node.pipe(stream).pipe(node);
+});
+
+function say_all(data) {
+	nodes.forEach(node.handle_speak.bind(node, data));
+}
+
+function add_user(node) {
+	nodes.push(node);
+}
+
+function remove_user(node) {
+	nodes = nodes.filter(function(n) {
+		return n !== node;
+	})
+}
+
+// Installing dnode
+sock.install(server, '/speakeasy');
+
+console.log("Listening");
 app.listen(process.env.PORT || 8080);

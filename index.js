038bbd1a89e4c381c0461931bc12259a12c7d498
RangerMauve <RangerMauve@hotmail.com>
2014-10-08 20:26:11 -0400

    Okay, screw socket.io

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 var express = require("express");
 var app = express();
 var server = require('http').Server(app);
-var fs = require('fs');
 
 app.disable('x-powered-by');
 
@@ -11,21 +10,6 @@ app.use(function(req, res, next) {
 	res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
 	res.header('Access-Control-Allow-Credentials', false);
 	next();
-});
-
-var io = require('socket.io').listen(server, {
-	origins: "*"
-});
-
-io.on('connection', function(socket) {
-	socket.on('speak', function(data) {
-		var voice = data.voice
-		var message = "" + data.message;
-		socket.broadcast.emit("speaking", {
-			voice: voice,
-			message: message
-		});
-	});
 });
 
 console.log("Listening")
